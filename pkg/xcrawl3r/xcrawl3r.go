81faba69ed185713d114590df864bcd676f2be63
Alex (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2025-04-06 15:15:50 +0300

    refactor(pkg): update the URLsToFilesRegex

--- pkg/xcrawl3r/xcrawl3r.go
+++ pkg/xcrawl3r/xcrawl3r.go
@@ -372,8 +372,7 @@ func New(cfg *Configuration) (crawler *Crawler, err error) {
 
 	crawler.URLFilterRegex = regexp.MustCompile(URLFilterRegexPattern)
 	crawler.URLsNotToRequestRegex = regexp.MustCompile(`(?i)\.(apng|bpm|png|bmp|gif|heif|ico|cur|jpg|jpeg|jfif|pjp|pjpeg|psd|raw|svg|tif|tiff|webp|xbm|3gp|aac|flac|mpg|mpeg|mp3|mp4|m4a|m4v|m4p|oga|ogg|ogv|mov|wav|webm|eot|woff|woff2|ttf|otf|css)(?:\?|#|$)`)
-	//nolint:gocritic,regexpSimplify
-	crawler.URLsToFilesRegex = regexp.MustCompile(`(?m).*?\.*(js|json|xml|csv|txt|map)(\?.*?|)$`)
+	crawler.URLsToFilesRegex = regexp.MustCompile(`(?i)\.(js|json|xml|csv|txt|map)(?:\?|#|$)`)
 
 	crawler.PageCollector = colly.NewCollector(
 		colly.Async(true),
